4746595c581547edd1879ffed533da411db84894
Kadali Mohan Kiran <mohankiran.k2004@gmail.com>
2025-12-06 22:54:47 +0530

    Fixed chatbot UI: moved to sidebar

--- UI/utils.py
+++ UI/utils.py
@@ -290,5 +290,5 @@ def embed_chatbot():
         (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="ZAWM2yGNRx2n7ggVz-fDk";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
         </script>
         """,
-
+        height=500, 
     )
